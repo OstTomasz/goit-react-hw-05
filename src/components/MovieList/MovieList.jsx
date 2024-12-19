fc43cd2b5b85edfb3797e6c4940d102703b91e95
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-12-19 12:11:17 +0100

    lkjgh

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 import { useGetMovies } from "../../Hooks/useGetMovies";
+import { useSearch } from "../../Hooks/useSearch";
 
 import css from "./MovieList.module.css";
+import { useState } from "react";
 
 export const MovieList = () => {
+  const [renderSource, setrenderSource] = useState(null);
+  const { search, loading: searchLoading, error: searchError } = useSearch();
+
   const { moviesList, error, loading } = useGetMovies();
 
   const location = useLocation();
 
-  if (loading) {
+  if (loading || searchLoading) {
     return <p>Loading...</p>;
   }
-  if (error) {
+  if (error || searchError) {
     return <p>Error: {error.message}</p>;
   }
 
--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,22 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import { useSearch } from "../../Hooks/useSearch";
 import css from "./Searcher.module.css";
 
 export const Searcher = () => {
-  // const [query, setQuery] = useState();
+  const [query, setQuery] = useState();
   // const search = useSearch(query);
 
   // const handleChange = (e) => {
   //   setQuery(e.target.value);
   // };
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  };
   return (
-    <form
-      className={css.form}
-      // onSubmit={handleSubmit}
-    >
+    <form className={css.form} onSubmit={handleSubmit}>
       <input
         className={css.input}
         // onChange={handleChange}
